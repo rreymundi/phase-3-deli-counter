43b574b0c455b67c6340a10168630caa882a8568
Raúl Reymundi <raul@kickstarter.com>
2022-09-26 11:32:50 -0400

    commit tree 6610a576aa50678994337acce5aa532056905e9d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(line)
+    if line.empty?
+      puts "The line is currently empty."
+    else
+      current_line = "The line is currently:"
+      line.each.with_index(1) do |person, i|
+        current_line << " #{i}. #{person}"
+      end
+      puts current_line
+    end
+  end
+
+def take_a_number(line, person)
+    line << person
+    puts "Welcome, #{person}. You are number #{line.length} in line."
+  end
+
+def now_serving(line)
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{line.first}."
+        line.shift
+    end
+end
